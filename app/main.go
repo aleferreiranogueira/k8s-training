45a4127ca7ef04ba77bf1fc81a4bd9712daaefce
Alexandre Ferreira <alexandre.ferreira@idwall.co>
2023-04-27 13:50:41 -0300

    chore: add remaining features

--- app/main.go
+++ app/main.go
@@ -1,30 +1,44 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gorilla/mux"
-	"github.com/redis/go-redis/v9"
 )
 
-var ctx = context.Background()
-var rdb = redis.NewClient(&redis.Options{
-	Addr:     os.Getenv("REDIS_HOST"),
-	Password: "", // no password set
-	DB:       0,  // use default DB
-})
+var up bool
 
 func list(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome home!")
 }
 
+func healthCheck(w http.ResponseWriter, r *http.Request) {
+	if os.Getenv("API_HOST") == "" {
+		w.WriteHeader(http.StatusServiceUnavailable)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
 func main() {
-	fmt.Printf("Init API")
+	up = false
+	fmt.Printf("Initializing...\n")
+
+	timeout := time.After(10 * time.Second)
+
+	select {
+	case <-timeout:
+		fmt.Printf("App is Up!\n")
+		up = true
+	}
+
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", list)
+	router.HandleFunc("/health", healthCheck)
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
